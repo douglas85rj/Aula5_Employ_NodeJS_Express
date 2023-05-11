c37b9a4c74007689cd4bd6d4d82145037cce8a4e
douglas85rj <douglas85rj@gmail.com>
2023-05-11 20:28:53 -0300

    update_delete

--- backEMS/controllers/employesController.js
+++ backEMS/controllers/employesController.js
@@ -1,31 +1,51 @@
-const { v4:uuidv4} = require('uuid')
+const { v4: uuidv4 } = require("uuid");
 
-let employes = []
+let employes = [];
 
 const getAllEmployes = (req, res) => {
-  res.status(200).json(employes)
-}
+  res.status(200).json(employes);
+};
 const addEmployes = (req, res) => {
-  const employe = req.body
-
-  employe.id = uuidv4()
-  employes.push(employe)
-  res.status(200).json(employe)
-}
+  const employe = req.body;
+  try {
+    employe.id = uuidv4();
+    employes.push(employe);
+    res.status(200).json(employe);
+  } catch {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const updateEmployes = (req, res) => {
-  const employe = req.body
-  res.status(200).json(employe)
-}
+  const id = req.params.id;
+  const employe = req.body;
+  try {
+    const index = employes.findIndex((e)=> e.id === id);
+    if (index === -1) {
+      return res.status(404).json({ message: "Employe not found" });
+    }
+    employes[index] = { ...employes[index], ...employe };
+    res.status(200).json(employes[index]);n
+   
+  } catch {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const deleteEmployes = (req, res) => {
-  const employe = req.body
-  res.status(200).json(employe)
-}
+  const id = req.params.id;
+  try {
+    employes = employes.filter((e) => e.id !== id);
+    res.status(200).json({ message: "Employe deleted successfully" });
+  } catch {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = {
   getAllEmployes,
   addEmployes,
   updateEmployes,
   deleteEmployes,
-}
+};
--- backEMS/index.js
+++ backEMS/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const employeRoutes = require('./routes/employeRoutes');
 
+const host = '127.0.0.1';
 const port = 3333;
 
 app.use (express.json());
 app.use ('/employes', employeRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running at http://${host}:${port}`);
 });
