0a5066dd06665f2edbe6e458db1c192932c1a9a6
douglas85rj <douglas85rj@gmail.com>
2023-05-11 10:45:08 -0300

    Erro_

--- backEMS/controllers/employesController.js
+++ backEMS/controllers/employesController.js
@@ -3,22 +3,29 @@ const { v4: uuidv4 } = require("uuid");
 let employes = [];
 
 const getAllEmployes = (req, res) => {
-  res.status(200).json({ employes });
+  res.status(200).json(employes);
+};
+const addEmployes = (req, res) => {
+  const employe = req.body;
 
-  const addEmploye = (req, res) => {
-    const employe = req.body;
-    employe.id = uuidv4();
-    employes.push(employes);
-    res.status(200).json({ employe });
-  };
+  employe.id = uuidv4();
+  employe.push(employe);
+  res.status(200).json(employe);
 };
 
 const updateEmployes = (req, res) => {
   const employe = req.body;
-  res.status(200).json({ employes });
+  res.status(200).json(employe);
 };
 
 const deleteEmployes = (req, res) => {
   const employe = req.body;
-  res.status = req.body;
+  res.status(200).json(employe);
+};
+
+module.exports = {
+  getAllEmployes,
+  addEmployes,
+  updateEmployes,
+  deleteEmployes,
 };
--- backEMS/routes/employeRoutes.js
+++ backEMS/routes/employeRoutes.js
@@ -6,8 +6,7 @@ const employesController = require ('../controllers/employesController')
 router.get( ('/', employesController.getAllEmployes) ) // Get all employes  
 router.post( ('/add', employesController.addEmployes) ) // Add an employes
 router.put( ('/update/:id', employesController.updateEmployes) ) // Update an employes
-router.delete( ('/delete/:id', employesController.deleteEmployes) ) // Delete an employes   
-   
+router.delete( ('/delete/:id', employesController.deleteEmployes) ) // Delete an employes     
 
 
 module.exports = router;
