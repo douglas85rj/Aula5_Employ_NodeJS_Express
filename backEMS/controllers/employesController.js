d2768690295868713bbd76fce906b3f3c7928126
douglas85rj <douglas85rj@gmail.com>
2023-05-11 11:07:13 -0300

    ident

--- backEMS/controllers/employesController.js
+++ backEMS/controllers/employesController.js
@@ -1,31 +1,31 @@
-const { v4:uuidv4} = require('uuid');
+const { v4:uuidv4} = require('uuid')
 
-let employes = [];
+let employes = []
 
 const getAllEmployes = (req, res) => {
-  res.status(200).json(employes);
+  res.status(200).json(employes)
 };
 const addEmployes = (req, res) => {
-  const employe = req.body;
+  const employe = req.body
 
-  employe.id = uuidv4();
-  employe.push(employe);
-  res.status(200).json(employe);
-};
+  employe.id = uuidv4()
+  employe.push(employe)
+  res.status(200).json(employe)
+}
 
 const updateEmployes = (req, res) => {
-  const employe = req.body;
-  res.status(200).json(employe);
-};
+  const employe = req.body
+  res.status(200).json(employe)
+}
 
 const deleteEmployes = (req, res) => {
-  const employe = req.body;
-  res.status(200).json(employe);
-};
+  const employe = req.body
+  res.status(200).json(employe)
+}
 
 module.exports = {
   getAllEmployes,
   addEmployes,
   updateEmployes,
   deleteEmployes,
-};
+}
